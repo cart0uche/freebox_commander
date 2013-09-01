9d58baf2b1e2cf91d85540ac2fc20687aa429d43
Hafid Saou <hafid.saou@gmail.com>
2013-09-02 00:30:29 +0200

    simplification

--- freebox_commander.py
+++ freebox_commander.py
@@ -16,7 +16,6 @@ class FreeboxCommander(Cmd):
 	def __init__(self):
 		self._remote_path = ["Disque dur"]
 		self._current_file_list = []
-		self._current_directories = []
 		Cmd.__init__(self)
 
 
@@ -52,7 +51,6 @@ class FreeboxCommander(Cmd):
 			print 'Login failed'
 			return
 
-		self.build_current_directories()
 		self.update_prompt()
 
 
@@ -67,7 +65,7 @@ class FreeboxCommander(Cmd):
 					print "%s  %5dMo %s"  %  ("d" if file["type"]=="dir" else "-", file["size"]/BYTE_PER_MO, file["name"])
 				else:
 					print file['name']
-				self._current_file_list.append(file['name'])
+				self._current_file_list.append(file)
 
 
 	def do_cd(self, args):
@@ -82,12 +80,10 @@ class FreeboxCommander(Cmd):
 		elif args == "..":
 			if len(self._remote_path) != 0:
 				self._remote_path.pop()
-				self.build_current_directories()
 				self.update_prompt()
 		else:
 			self._remote_path.append(args)
 			if self._fb.get_file_list(self.current_path_name)['success'] is True:
-				self.build_current_directories()
 				self.update_prompt()
 			else:
 				self._remote_path.pop()
@@ -95,7 +91,8 @@ class FreeboxCommander(Cmd):
 
 
 	def complete_cd(self, text, line, begidx, endidx):
-		return  [i for i in self._current_directories if i.lower().startswith(text.lower())]
+		current_directories = self.build_current_files("dir")
+		return  [i for i in current_directories if i.lower().startswith(text.lower())]
 
 
 	def do_get(self, args):
@@ -103,13 +100,14 @@ class FreeboxCommander(Cmd):
 		if self.is_connected() is False:
 			return
 
-		for file in self._current_file_list['result']:
-			if file['name'] == args:
+		for file in self._current_file_list:
+			if file["name"] == args:
 				self._fb.download_file(file['path'], file['name'])
 
 
 	def complete_get(self, text, line, begidx, endidx):
-		return  [i for i in self._current_file_list if i.lower().startswith(text.lower())]
+		current_files = self.build_current_files("file")
+		return  [file for file in current_files if file.lower().startswith(text.lower())]
 
 
 	def do_quit(self, args):
@@ -126,11 +124,12 @@ class FreeboxCommander(Cmd):
 			return False
 
 
-	def build_current_directories(self):
-		del self._current_directories[:]
+	def build_current_files(self, type):
+		current_files = []
 		for file in  self._fb.get_file_list(self.current_path_name)['result']:
-			if file["type"] == "dir" and file["name"] not in [".", ".."]:
-				self._current_directories.append(file['name'].encode('utf-8'))
+			if file["type"] == type and file["name"] not in [".", ".."]:
+				current_files.append(file['name'].encode('utf-8'))
+		return current_files
 
 
 	def update_prompt(self):
